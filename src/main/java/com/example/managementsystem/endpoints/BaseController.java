b5b47e78f9473f9445541d02c305aa5f95e467a4
Bálint <balint.gyorffy@gmail.com>
2022-03-28 23:38:03 +0200

    allow headers

--- src/main/java/com/example/managementsystem/endpoints/BaseController.java
+++ src/main/java/com/example/managementsystem/endpoints/BaseController.java
@@ -3,7 +3,9 @@ package com.example.managementsystem.endpoints;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Slf4j
 public class BaseController {
 
