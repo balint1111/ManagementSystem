bb8b34c95b375f0061d3ad36b411d6caa7b48da2
Bálint <balint.gyorffy@gmail.com>
2022-04-24 01:20:48 +0200

    fix

--- src/main/java/com/example/managementsystem/endpoints/BaseController.java
+++ src/main/java/com/example/managementsystem/endpoints/BaseController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Slf4j
